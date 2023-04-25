d502bda6efa3063286577f3a15903039cc1e20fe
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-04-25 16:09:06 +0200

    collect more data

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def compute_bb(asks: list, bids: list, mid_price: float, percent: int):
 
     # Compute the book bias
     book_bias = (total_bid_quantity - total_ask_quantity) / (total_bid_quantity + total_ask_quantity)
-    return book_bias
+    return total_ask_quantity, total_bid_quantity, book_bias
 
 
 compute_bb05 = partial(compute_bb, percent=0.005)
@@ -60,10 +60,10 @@ async def main():
                 bids = ob.get_bids()
                 mid_price = (asks[0][0] + bids[0][0]) / 2
 
-                bb05 = compute_bb05(asks, bids, mid_price)
-                bb1 = compute_bb1(asks, bids, mid_price)
-                bb2 = compute_bb2(asks, bids, mid_price)
-                bb4 = compute_bb4(asks, bids, mid_price)
+                ask_d05, bid_d05, bb05 = compute_bb05(asks, bids, mid_price)
+                ask_d1, bid_d1, bb1 = compute_bb1(asks, bids, mid_price)
+                ask_d2, bid_d2, bb2 = compute_bb2(asks, bids, mid_price)
+                ask_d4, bid_d4, bb4 = compute_bb4(asks, bids, mid_price)
                 logger.info(f"BB4 at {current_time}: {bb4:.2f}")
                 logger.info(f"Total asks: {len(asks)}")
                 logger.info(f"Total bids: {len(bids)}")
@@ -71,6 +71,16 @@ async def main():
                 # Insert the book biases and mid-price into MongoDB
                 book_bias_data = {
                     "mid_price": mid_price,
+                    "best_bid": bids[0][0],
+                    "best_ask": asks[0][0],
+                    "total_ask_05": ask_d05,
+                    "total_ask_1": ask_d1,
+                    "total_ask_2": ask_d2,
+                    "total_ask_4": ask_d4,
+                    "total_bid_05": ask_d05,
+                    "total_bid_1": ask_d1,
+                    "total_bid_2": ask_d2,
+                    "total_bid_4": ask_d4,
                     "book_bias_05": bb05,
                     "book_bias_1": bb1,
                     "book_bias_2": bb2,
