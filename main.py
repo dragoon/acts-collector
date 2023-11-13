2ae6e92b278101a50b909e9a948b304fd31f72f3
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-11-13 11:52:03 +0100

    add resilient error handling

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from pymongo.server_api import ServerApi
 
 from settings import MONGO_URI, configure_logging
 
-
 parser = argparse.ArgumentParser(description="Binance order book collector")
 parser.add_argument("-s", "--symbol", required=True, help="Instrument code to collect data for, must be USDT pair")
 args = parser.parse_args()
@@ -36,66 +35,92 @@ def compute_bb(asks: list, bids: list, mid_price: float, percent: int):
     return total_ask_quantity, total_bid_quantity, book_bias
 
 
-compute_bb05 = partial(compute_bb, percent=0.005)
 compute_bb1 = partial(compute_bb, percent=0.01)
-compute_bb2 = partial(compute_bb, percent=0.02)
 compute_bb4 = partial(compute_bb, percent=0.04)
 
 
 async def main():
-    client = await AsyncClient.create()
-    dcm = DepthCacheManager(client, symbol=f'{symbol.upper()}USDT',
-                            limit=5000,  # initial number of order from the order book
-                            refresh_interval=0,  # disable cache refresh
-                            ws_interval=100   # update interval on websocket, ms
-                            )
-    logger.info(f"Starting order book collection for {symbol}-USDT")
+    max_retries = 5
+    retry_delay = 1  # Initial backoff delay in seconds
+    retry_count = 0
 
-    async with dcm as dcm_socket:
-        last_stored_minute = None
-        while True:
-            ob = await dcm_socket.recv()
-            current_time = datetime.utcnow()
+    while True:
+        client = None
+        try:
+            client = await AsyncClient.create()
+            dcm = DepthCacheManager(client, symbol=f'{symbol.upper()}USDT',
+                                    limit=5000,  # initial number of order from the order book
+                                    refresh_interval=0,  # disable cache refresh
+                                    ws_interval=100  # update interval on websocket, ms
+                                    )
+            logger.info(f"Starting order book collection for {symbol}-USDT")
 
-            current_minute = current_time.minute
-            if current_minute != last_stored_minute:
-                # Get the asks and bids
-                asks = ob.get_asks()
-                bids = ob.get_bids()
-                mid_price = (asks[0][0] + bids[0][0]) / 2
+            async with dcm as dcm_socket:
+                last_stored_minute = None
+                while True:
+                    ob = await dcm_socket.recv()
+                    current_time = datetime.utcnow()
 
-                ask_d05, bid_d05, bb05 = compute_bb05(asks, bids, mid_price)
-                ask_d1, bid_d1, bb1 = compute_bb1(asks, bids, mid_price)
-                ask_d2, bid_d2, bb2 = compute_bb2(asks, bids, mid_price)
-                ask_d4, bid_d4, bb4 = compute_bb4(asks, bids, mid_price)
-                logger.info(f"BB4 at {current_time}: {bb4:.2f}")
-                logger.info(f"Total asks: {len(asks)}")
-                logger.info(f"Total bids: {len(bids)}")
+                    current_minute = current_time.minute
+                    if current_minute != last_stored_minute:
+                        # Get the asks and bids
+                        asks = ob.get_asks()
+                        bids = ob.get_bids()
+                        mid_price = (asks[0][0] + bids[0][0]) / 2
 
-                # Insert the book biases and mid-price into MongoDB
-                book_bias_data = {
-                    "mid_price": mid_price,
-                    "best_bid": bids[0][0],
-                    "best_ask": asks[0][0],
-                    "total_ask_05": ask_d05,
-                    "total_ask_1": ask_d1,
-                    "total_ask_2": ask_d2,
-                    "total_ask_4": ask_d4,
-                    "total_bid_05": bid_d05,
-                    "total_bid_1": bid_d1,
-                    "total_bid_2": bid_d2,
-                    "total_bid_4": bid_d4,
-                    "book_bias_05": bb05,
-                    "book_bias_1": bb1,
-                    "book_bias_2": bb2,
-                    "book_bias_4": bb4,
-                    "last_book_update": datetime.fromtimestamp(ob.update_time/1000),
-                    "current_time": current_time
-                }
-                book_bias_collection.insert_one(book_bias_data)
+                        ask_d1, bid_d1, bb1 = compute_bb1(asks, bids, mid_price)
+                        ask_d4, bid_d4, bb4 = compute_bb4(asks, bids, mid_price)
+                        logger.info(f"BB4 at {current_time}: {bb4:.2f}")
+                        logger.info(f"Total asks: {len(asks)}")
+                        logger.info(f"Total bids: {len(bids)}")
 
-                # Update the last stored minute
-                last_stored_minute = current_minute
+                        # Insert the book biases and mid-price into MongoDB
+                        book_bias_data = {
+                            "mid_price": mid_price,
+                            "best_bid": bids[0][0],
+                            "best_ask": asks[0][0],
+                            "total_ask_1": ask_d1,
+                            "total_ask_4": ask_d4,
+                            "total_bid_1": bid_d1,
+                            "total_bid_4": bid_d4,
+                            "book_bias_1": bb1,
+                            "book_bias_4": bb4,
+                            "last_book_update": datetime.fromtimestamp(ob.update_time / 1000),
+                            "current_time": current_time
+                        }
+                        book_bias_collection.insert_one(book_bias_data)
+
+                        # Update the last stored minute
+                        last_stored_minute = current_minute
+                        retry_count = 0
+
+        except asyncio.CancelledError:
+            # This is likely due to a shutdown signal
+            logger.info("Shutdown signal received, closing client and exiting...")
+            break
+
+        except asyncio.TimeoutError as e:
+            logger.error(f"Network error: {e}. Reconnecting...")
+            await asyncio.sleep(retry_delay)
+
+        except Exception as e:
+            logger.error(f"An unexpected error occurred: {e}")
+            retry_count += 1
+            if retry_count > max_retries:
+                logger.error("Max retries exceeded. Exiting...")
+                break
+
+            # Exponential backoff
+            wait = retry_delay * 2 ** min(retry_count, max_retries)
+            logger.info(f"Attempting to reconnect in {wait} seconds...")
+            await asyncio.sleep(wait)
+            continue  # Continue the while loop to retry connection
+
+        finally:
+            # Perform cleanup
+            if client is not None:
+                await client.close_connection()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
